f0db40f21f9e74b7bbff86a0e26005ebefbc64c6
Dany Varghese <57636833+danyvarghese83@users.noreply.github.com>
2020-07-29 13:02:07 -0500

    Add files via upload

---
+++ client/build/precache-manifest.664df432a66f85830a81d9c514200068.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c1163c7d1f87134a1ec0ad644ad1f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ec4ccfc936aa40d746",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aecb30a40680ba08acff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91ec4ccfc936aa40d746",
+    "url": "/static/js/2.d4ab44e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4ab44e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecb30a40680ba08acff",
+    "url": "/static/js/main.750952bb.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
